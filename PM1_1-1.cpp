626094c7fcabedcfb7d0ee706f53018f38e952c4
currycafe <115197484+currycafe@users.noreply.github.com>
2022-11-18 09:47:45 +0900

    リモートリポジトリでコンフリクト

--- PM1_1-1.cpp
+++ PM1_1-1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "Git Hub Clone Changed\n";
+    std::cout << "Git Hub Clone Change\n";
     std::cout << "New Branch Change\n";
 
     std::cout << "Hello VisualStudio Git!\n";
